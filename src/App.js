0984e3cbf236c4e084bd62ab2e44ba7d87b8f519
Tony Weber <weber.tony@gmail.com>
2021-07-11 08:23:01 -0700

    test commit for basic react app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        Amplify tutorial coming...
         <p>
           Edit <code>src/App.js</code> and save to reload. Got it.--TW
         </p>
